493f8c80096f4cc3289b2c210ea926a84c3743d9
cseitz5 <cseitz5@kent.edu>
2020-11-14 18:22:20 -0500

    Added fake API folder and 404 page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,6 +17,14 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
+
+// Put the 404 not found route at the end.
+// This should ALWAYS be the last route, as it is a wildcard.
+routes.push({
+  path: '/:catchAll(.*)',
+  name: 'NotFound',
+  component: NotFound,
+})
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="notfound">
+    404 Not Found
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NotFound',
+  components: {
+
+  }
+});
+</script>
