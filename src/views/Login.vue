8eb4c7c9d285d36faf3ea65acba9bb67a588a4b2
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-01-04 16:57:20 -0500

    Login page created

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    VERIFY WITH DISCORD
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/scss/views/Login.scss";
+
+
+</style>
