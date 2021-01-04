989d75de602e216c98fb5009b50ca0cfd0766d5a
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-01-04 17:53:42 -0500

    WIP user

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {
+      hideNavigation: true,
+    },
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <User userId="0"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import User from '@/components/User.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    User,
   },
 };
 </script>
