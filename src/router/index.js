3022859102e080150b7c29f79f376a0ca3b1b700
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-03-01 10:51:50 -0500

    fresh restart

--- src/router/index.js
+++ src/router/index.js
@@ -1,116 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Checkin from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Logout from '../views/Logout.vue'
-import NotFound from '../views/NotFound.vue'
-
-import store from '@/store'
-
-let loginRequired = {
-  beforeEnter: (to, from, next) => {
-    // ensure we are logged in, otherwise redirect
-    if (store.getters.isLoggedIn) {
-      next();
-    } else {
-      next({
-        name: 'Login', query: {
-          redirect: String(to.name),
-        }
-      })
-    }
-  },
-};
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    meta: {
-      title: 'Staff'
-    },
-    component: Home,
-    ...loginRequired,
+    component: Home
   },
   {
-    path: '/checkin',
-    name: 'Checkin',
-    meta: {
-      title: 'Staff - Checkin'
-    },
-    component: Checkin,
-    ...loginRequired,
-  },
-  {
-    path: '/checkin',
-    name: 'Checkin',
-    meta: {
-      title: 'Staff - Checkin'
-    },
-    component: Checkin,
-    ...loginRequired,
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    meta: {
-      title: 'Staff - Login',
-      hideNavigation: true,
-    },
-    component: Login,
-    beforeEnter: (to, from, next) => {
-      // if we are logged in, redirect to query-specified OR home
-      if (store.getters.isLoggedIn) {
-        if (to.query.redirect) {
-          // convert to camel case. anything casing beyond first letter must be done in URL.
-          // this code converts "apply" -> "Apply".
-          // it isn't a perfect solution, but for MOST routes, named components are 1 word
-          //   and this will allow lowercase url redirect paramater
-          const CamelCased = String(to.query.redirect).replace(/^./, str => str.toUpperCase());
-          next({ name: CamelCased, }) // redirect to named component
-        } else {
-          next({ name: 'Home', }) // redirect to dashboard
-        }
-      } else {
-        next() // not logged in, continue to authentication
-      }
-    },
-  },
-  {
-    path: '/logout',
-    name: 'Logout',
-    meta: {
-      title: 'Staff - Logged Out',
-      hideNavigation: true,
-    },
-    component: Logout,
-    beforeEnter: (to, from, next) => {
-      if (store.getters.isLoggedIn) {
-        store.dispatch('logout').then(function() {
-          next();
-        });
-      } else {
-        next();
-      }
-    },
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
-
-// Put the 404 not found route at the end.
-// This should ALWAYS be the last route, as it is a wildcard.
-routes.push({
-  path: '/:catchAll(.*)',
-  name: 'NotFound',
-  component: NotFound,
-})
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
-
-router.afterEach((to) => {
-  document.title = to.meta.title || to.name || 'Kent Hack Enough';
 })
 
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,43 +1,11 @@
 import { createStore } from 'vuex'
 
-import router from '@/router';
-
 export default createStore({
   state: {
-    token: localStorage.getItem('token') || false,
-    status: '',
-    user: {},
   },
   mutations: {
-    fetchToken(state) {
-      state.token = localStorage.getItem('token') || false;
-    },
   },
   actions: {
-    async logout({ commit, state }) {
-      let req;
-      try {
-        req = await fetch('/api/staff/logout', {
-          headers: {
-            'Authorization': `Bearer ${state.token}`,
-          }
-        })
-      } catch (e) {
-        // idk what to do
-        console.error(e);
-        console.log(req);
-      } finally {
-        localStorage.removeItem('token');
-        commit('fetchToken')
-        router.push({
-          name: 'Logout',
-        })
-      }
-    },
-  },
-  getters: {
-    isLoggedIn: state => !!state.token,
-    authStatus: state => state.status,
   },
   modules: {
   }
